657f2167d7e5dcb64150d396d5dc470bd9fbb64f
Timothy Lim <timothylim.student@gmail.com>
2020-10-22 16:13:11 -0300

    Done with Basic Card Game

--- App/index.js
+++ App/index.js
@@ -46,7 +46,36 @@ const styles = StyleSheet.create({
   },
 });
 
+const getColumnOffset = (index) => {
+  switch (index) {
+    case 0:
+      return 1.2;
+    case 1:
+      return 0;
+    case 2:
+      return -1.2;
+    default:
+      return 0;
+  }
+};
+
 class Card extends React.Component {
+  offset = new Animated.Value(CARD_WIDTH * getColumnOffset(this.props.index));
+
+  componentDidMount() {
+    this.timeout = setTimeout(() => {
+      Animated.timing(this.offset, {
+        toValue: 0,
+        duration: 250,
+        useNativeDriver: true,
+      }).start();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
   render() {
     const { onPress, image, isVisible } = this.props;
     let displayImage = (
@@ -63,9 +92,19 @@ class Card extends React.Component {
       );
     }
 
+    const animationStyles = {
+      transform: [
+        {
+          translateX: this.offset,
+        },
+      ],
+    };
+
     return (
       <TouchableOpacity onPress={onPress}>
-        <View style={styles.card}>{displayImage}</View>
+        <Animated.View style={[styles.card, animationStyles]}>
+          {displayImage}
+        </Animated.View>
       </TouchableOpacity>
     );
   }
@@ -91,7 +130,7 @@ class Row extends React.Component {
   opacity = new Animated.Value(0);
 
   componentDidMount() {
-    setTimeout(() => {
+    this.timeout = setTimeout(() => {
       Animated.parallel([
         Animated.timing(this.offset, {
           toValue: 0,
@@ -105,6 +144,10 @@ class Row extends React.Component {
         }),
       ]).start();
     }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
   }
 
   render() {
@@ -239,6 +282,7 @@ class App extends React.Component {
                 return (
                   <Card
                     key={cardId}
+                    index={index}
                     onPress={() => this.handleCardPress(cardId, card.image)}
                     image={card.image}
                     isVisible={
